db8bda94ee6090b81af0f1bff107591fb0d3209a
acosson <antho.cosson@gmail.com>
2022-07-07 16:22:57 +0200

    projet blue and green

---
+++ BlueGreenFragments/app/src/main/java/fr/wololobzh/blue_green_fragments/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.wololobzh.blue_green_fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
