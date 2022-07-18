8e4f7429087d70de0ef12409c1e1be0dde2cae7a
cosson <acosson@eni-ecole.fr>
2022-07-18 15:07:12 +0200

    init chap14

--- RetrofitChuckNorris/app/src/main/java/fr/acos/retrofitchucknorris/entities/Joke.kt
+++
@@ -1,9 +0,0 @@
-package fr.acos.retrofitchucknorris.entities
-
-import com.squareup.moshi.Json
-
-data class Joke(
-    val id: String,
-    @Json(name = "icon_url") val image: String,
-    @Json(name = "value") val joke: String
-)
